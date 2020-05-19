b43dc3163c368f10bd7767797334dbfd86fa20ca
eduardobrandao <edu.slz.sud@gmail.com>
2020-05-19 10:21:34 -0300

    ainda estava comentado o import da api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import Header from "./Header";
 import "./style.css";
 import "./global.css";
-//import api from "./api";
+import api from "./api";
 import Main from "./pages";
 import Cadastro from "./Cadastro";
 
 const App = () => (
-  <div className="App">
-    <Header />
-    <Main />
-    <Cadastro />
-  </div>
+    <div className="App">
+        <Header />
+        <Main />
+        <Cadastro />
+    </div>
 );
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import api from "./api";
 import Home from "./Home";
 import Cadastro from "./cadastro.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact={true} component={App} />
-      <Route path="/home" component={Home} />
-      <Route path="/cadastro" component={Cadastro} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/home" component={Home} />
+            <Route path="/cadastro" component={Cadastro} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
